fe10b96b333f33ce69f2f92c56482a8d07de0f8f
himanshukandari14 <himanshukandari14@gmail.com>
2024-12-01 20:34:40 +0530

    Tailwind setup

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,15 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
-const api = {}
+if(!process.contextIsolated){
+  throw new Error('Contextisoloation must be enabled inbrowser window')
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
-} else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI
-  // @ts-ignore (define in dts)
-  window.api = api
+}
+
+try{
+  contextBridge.exposeInMainWorld('context', {
+    // TODO
+  })
+} catch (error) {
+  console.log(error)
 }
